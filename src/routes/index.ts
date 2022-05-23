50094b4470cfaaf72479a7b7b60d88b45e7d6d32
CyberValeriy <valeriyhrynchyshyn@gmail.com>
2022-05-23 14:47:13 +0300

    Add test route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import AuthRouter from "./auth.router";
 import CommonRouter from "./common.router";
+import TestRouter from "./test.router";
 
 const apiRouter = Router();
 
 apiRouter.use("/auth", AuthRouter);
 apiRouter.use("/common", CommonRouter);
+apiRouter.use("/test", TestRouter);
 
 export default apiRouter;
