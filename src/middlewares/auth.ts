6cb9932e915cd696c1d6f49c3f65c224fcb887e0
CyberValeriy <valeriyhrynchyshyn@gmail.com>
2022-05-23 17:25:36 +0300

    Add fix for middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -8,19 +8,19 @@ import CONFIG from "../config";
 export const auth = async (req: IAuth, res: Response, next: NextFunction) => {
   const authHeader = req.get("Authorization");
   if (!authHeader) {
-    return next(/*custom err*/);
+    return next(ApiError.UnauthorizedError("No auth header!"));
   }
 
   try {
     const token = authHeader.split(" ")[1];
-    if (!token) return next(ApiError.UnauthorizedError());
+    if (!token) return next(ApiError.UnauthorizedError("No token!"));
 
     const verifiedData: any = jwt.verify(token, CONFIG.JWT.SECRET);
     const { sub } = verifiedData;
 
     const student = await StudentModel.findOne({ email: sub });
     if (!student) {
-      return next(ApiError.ForbiddenError());
+      return next(ApiError.ForbiddenError("Student not exist!"));
     }
 
     req.student = student;
